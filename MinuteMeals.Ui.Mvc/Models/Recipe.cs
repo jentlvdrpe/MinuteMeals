f4487f984ee2414f3266666cd1ba1ea678457c22
jentlvdrpe <jentlvandorpe@hotmail.com>
2024-10-28 18:34:35 +0100

    Extra page aangemaakt. Klaar om lijst toe te voegen.

---
+++ MinuteMeals.Ui.Mvc/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinuteMeals.Ui.Mvc.Models
+{
+    [Table(nameof(Recipe))]
+    public class Recipe
+    {
+        public required string Name { get; set; }
+        public required string Ingredients { get; set; }
+        public required string Steps { get; set; }
+        public int CookingTimeInMinutes { get; set; }
+    }
+}
